2fad3e81c0446189f2bb2f52311c56682e0d8797
Tied <thijstiedemann@gmail.com>
2021-03-21 11:15:39 +0100

    project set up

---
+++ local_scripts/setup-environment.sh
@@ -0,0 +1,10 @@
+echo Set up environment
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install -U pip
+pip install -r requirements/requirements.txt
+pip install -r requirements/requirements-dev.txt
+
